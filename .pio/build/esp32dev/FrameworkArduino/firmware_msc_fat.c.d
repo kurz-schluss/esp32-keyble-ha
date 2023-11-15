c72c906511e3bcf1962523df4b3fd3ff41cf43e8
kurz-schluss <github@homagk.de>
2023-11-15 15:12:33 +0100

    Update platformio.ini

--- .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /home/prof/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /home/prof/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
--- .pio/build/esp32dev/lib7cb/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/lib7cb/BLE/BLEExceptions.cpp.o: \
- /home/prof/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
