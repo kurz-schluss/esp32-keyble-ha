6071e74e9eae67b75f70008791eb9997f05b2431
kurz-schluss <github@homagk.de>
2023-11-15 14:58:07 +0100

    Test

---
+++ .pio/build/esp32dev/lib7cb/BLE/BLESecurity.cpp.d
@@ -0,0 +1,8 @@
+.pio/build/esp32dev/lib7cb/BLE/BLESecurity.cpp.o: \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.cpp \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.h \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
