702b688e66fe605595efc5126cc54711b33a217a
kurz-schluss <github@homagk.de>
2024-05-04 11:56:28 +0200

    Test

--- .pio/build/esp32dev/lib97b/Ticker/Ticker.cpp.d
+++
@@ -1,6 +0,0 @@
-.pio/build/esp32dev/lib97b/Ticker/Ticker.cpp.o: \
- /home/prof/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.cpp \
- /home/prof/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.h \
- /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
- /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
- /home/prof/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h
